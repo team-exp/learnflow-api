bf0cb113d5b174f59dbb0fd9d706b64b9c99a2d3
JinYong833 <jjystudy1@gmail.com>
2025-12-04 18:53:06 +0900

    feat[enrollment] : controller 구현 - create(수강 생성) 구현 - 현재 생성된 Enrollment의 모든 정보를 반환(테스트용)

--- src/main/java/com/teamexp/learnflowapi/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/teamexp/learnflowapi/enrollment/controller/EnrollmentController.java
@@ -1,4 +1,29 @@
 package com.teamexp.learnflowapi.enrollment.controller;
 
+import com.teamexp.learnflowapi.enrollment.dto.EnrollmentRequest;
+import com.teamexp.learnflowapi.enrollment.dto.EnrollmentResponse;
+import com.teamexp.learnflowapi.enrollment.service.EnrollmentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/enrollment")
 public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EnrollmentResponse> create(@Valid @RequestBody EnrollmentRequest request) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(enrollmentService.createEnrollment(request));
+    }
 }
