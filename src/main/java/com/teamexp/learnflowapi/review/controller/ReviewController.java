e791637d71136218ce32d58d70e63b7eecb58072
Sung Ryul Cho <hahahoho5811@gmail.com>
2025-12-06 00:40:28 +0900

    feat[review]: ReviewController: GET /lectures/{lectureId}, GET /my 엔드포인트 구현

--- src/main/java/com/teamexp/learnflowapi/review/controller/ReviewController.java
+++ src/main/java/com/teamexp/learnflowapi/review/controller/ReviewController.java
@@ -6,11 +6,12 @@ import com.teamexp.learnflowapi.review.dto.ReviewResponse;
 import com.teamexp.learnflowapi.review.service.ReviewService;
 import com.teamexp.learnflowapi.user.config.CustomUserPrincipal;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
@@ -25,10 +26,33 @@ public class ReviewController {
     public BaseResponse<ReviewResponse> createReview(
         @AuthenticationPrincipal CustomUserPrincipal principal,
         @Valid @RequestBody ReviewRequest request
-        ) {
+    ) {
         String userId = principal.getId();
 
         ReviewResponse response = reviewService.createReview(userId, request);
         return BaseResponse.ok(response);
     }
+
+    // 2. 강의별 리뷰 조회(공개 API)
+    @GetMapping("/lectures/{lectureId}")
+    public BaseResponse<Page<ReviewResponse>> getReviewsByLecture(
+        @PathVariable Long lectureId,
+        @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<ReviewResponse> response = reviewService.getReviewsByLecture(lectureId,pageable);
+        return BaseResponse.ok(response);
+    }
+
+
+    // 3. 내 리뷰 조회(마이페이지)
+    @GetMapping("/my")
+    public BaseResponse<Page<ReviewResponse>> getMyReviews(
+        @AuthenticationPrincipal CustomUserPrincipal principal,
+        @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        String userId = principal.getId();
+        Page<ReviewResponse> response = reviewService.getMyReviews(userId, pageable);
+        return BaseResponse.ok(response);
+    }
+
 }
