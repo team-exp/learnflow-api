16483656c09cc6dafe97b52d583d0f733e9efc09
JSeHoone <beebea545@gmail.com>
2025-12-06 01:39:09 +0900

    refactor : UserCreateRequest 어노테이션 추가
    - password, nickname 필드에 NotBlank 어노테이션을 추가하였습니다.

--- src/main/java/com/teamexp/learnflowapi/user/controller/dto/UserCreateRequest.java
+++ src/main/java/com/teamexp/learnflowapi/user/controller/dto/UserCreateRequest.java
@@ -1,6 +1,7 @@
 package com.teamexp.learnflowapi.user.controller.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserCreateRequest(
@@ -9,9 +10,11 @@ public record UserCreateRequest(
         String email,
 
         @NotNull(message = "Password cannot be null")
+        @NotBlank(message = "Password cannot be blank")
         String password,
 
         @NotNull(message = "Nickname cannot be null")
+        @NotBlank(message = "password cannot be blank")
         String nickname
 ) {
 
