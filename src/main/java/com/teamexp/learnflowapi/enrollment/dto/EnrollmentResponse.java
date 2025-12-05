a0b9a32b54de41e3391fceb4ee42b29b1fd4e335
JinYong833 <jjystudy1@gmail.com>
2025-12-05 16:08:40 +0900

    refactor[Enrollment] : EnrollmentResponse 시간 타입 협의 후 수정

--- src/main/java/com/teamexp/learnflowapi/enrollment/dto/EnrollmentResponse.java
+++ src/main/java/com/teamexp/learnflowapi/enrollment/dto/EnrollmentResponse.java
@@ -3,17 +3,15 @@ package com.teamexp.learnflowapi.enrollment.dto;
 import com.teamexp.learnflowapi.enrollment.model.Enrollment;
 import com.teamexp.learnflowapi.enrollment.model.EnrollmentStatus;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
+import java.time.Instant;
 
 public record EnrollmentResponse(
         Long userId,
         Long lectureId,
         EnrollmentStatus enrollmentStatus,
         Integer progress,
-        OffsetDateTime enrolledAt,
-        OffsetDateTime updatedAt
+        Instant enrolledAt,
+        Instant updatedAt
 ) {
     public static EnrollmentResponse from(Enrollment enrollment) {
         return new EnrollmentResponse(
@@ -21,7 +19,7 @@ public record EnrollmentResponse(
                 enrollment.getLectureId(),
                 enrollment.getStatus(),
                 enrollment.getProgress(),
-                enrollment.getEnrolledAt().atOffset(ZoneOffset.ofHours(9)).truncatedTo(ChronoUnit.SECONDS),
-                enrollment.getUpdatedAt().atOffset(ZoneOffset.ofHours(9)).truncatedTo(ChronoUnit.SECONDS));
+                enrollment.getEnrolledAt(),
+                enrollment.getUpdatedAt());
     }
 }
