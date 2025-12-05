32fccb781ca132ac495d36107b38559041b49aae
JSeHoone <beebea545@gmail.com>
2025-12-05 10:21:52 +0900

    refactor : UserDetails 상속

--- src/main/java/com/teamexp/learnflowapi/user/model/User.java
+++ src/main/java/com/teamexp/learnflowapi/user/model/User.java
@@ -9,14 +9,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.UUID;
+import lombok.Getter;
 import org.hibernate.annotations.SQLDelete;
+import java.util.List;
 
 import java.time.OffsetDateTime;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+@Getter
 @Entity
 @Table(name = "users")
 @SQLDelete(sql = "UPDATE users SET del_flag = true WHERE user_id = ?")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -58,7 +67,35 @@ public class User {
         this.delFlag = false;
     }
 
-    public static User createUser( String nickname, String email, String password, UserRole role) {
-        return new User(nickname, email, password, role);
+    public static User createUser( String email, String password, String nickname, UserRole role) {
+        return new User(email, password, nickname, role);
     }
+
+    // UserDetails의 메서드들
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
