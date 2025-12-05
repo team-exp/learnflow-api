c24043fc83648803f8e4d9a5d5f8485d66849006
JSeHoone <beebea545@gmail.com>
2025-12-05 10:29:59 +0900

    feat : JwtTokenProvider 추가
    - AccessToken 생성 로직 : Subject에는 userId, claim에는 email이랑 Role을 넣었습니다.
    
    - RefreshToken 생성 로직 : Subject에 userId만 넣었습니다.
    
    - parseToken 로직 : JWT 토큰을 검증하고(서명 확인), 토큰 안에 들어있는 Claims(페이로드 데이터)를 추출하는 메서드를 정의했습니다.

---
+++ src/main/java/com/teamexp/learnflowapi/user/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,63 @@
+package com.teamexp.learnflowapi.user.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-expiration}")
+    private long accessTokenValidityInMs;
+
+    @Value("${jwt.refresh-token-expiration}")
+    private long refreshTokenValidityInMs;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createAccessToken(String userId, String email, String role) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + accessTokenValidityInMs);
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))  // userId를 subject로
+                .claim("email", email)
+                .claim("role", role)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createRefreshToken(String userId) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + refreshTokenValidityInMs);
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
