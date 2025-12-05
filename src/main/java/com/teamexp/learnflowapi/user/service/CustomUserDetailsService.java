927c9ca6074a659802493023dd2d1225397e992c
JSeHoone <beebea545@gmail.com>
2025-12-05 10:22:47 +0900

    feat : CustomUserDetailService 추가

---
+++ src/main/java/com/teamexp/learnflowapi/user/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.teamexp.learnflowapi.user.service;
+
+import com.teamexp.learnflowapi.user.config.CustomUserPrincipal;
+import com.teamexp.learnflowapi.user.model.User;
+import com.teamexp.learnflowapi.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return new CustomUserPrincipal(user);
+    }
+}
